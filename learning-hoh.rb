ecfdcfca774398350e6017ebc0a295dfbf060570
Nory Lazo <nlazo@icloud.com>
2019-10-18 04:45:14 +0000

    Automatically backed up by Learn

--- learning-hoh.rb
+++ learning-hoh.rb
@@ -49,31 +49,31 @@
   # HoH all the way to Species!
   
   
-PORTION_1 = {
-  label: "Kingdom",
-  sub_category: {
-    label: "Phylum",
-    sub_category: {
-      label: "Class"
-    }
-  }
-}
+# PORTION_1 = {
+#   label: "Kingdom",
+#   sub_category: {
+#     label: "Phylum",
+#     sub_category: {
+#       label: "Class"
+#     }
+#   }
+# }
 
-PORTION_2 = {
-  label: "Order"
-}
+# PORTION_2 = {
+#   label: "Order"
+# }
 
-PORTION_3 = {
-  label: "Family",
-  sub_category: {
-    label: "Genus",
-  }
-}
+# PORTION_3 = {
+#   label: "Family",
+#   sub_category: {
+#     label: "Genus",
+#   }
+# }
 
-PORTION_4 = {
-  label: "Species",
-  sub_category: nil
-}
+# PORTION_4 = {
+#   label: "Species",
+#   sub_category: nil
+# }
 
 def naming_system
   
